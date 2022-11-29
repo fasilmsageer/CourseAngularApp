5da2e75dd6bd5bf21e7eac1d88488c77c3faa8f7
fasilmsageer <fasilmsageer@gmail.com>
2022-11-29 12:04:35 +0530

    api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ViewCourseComponent } from './view-course/view-course.component';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 const myRoute=[
   {
@@ -31,7 +32,8 @@ const myRoute=[
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(myRoute),
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
