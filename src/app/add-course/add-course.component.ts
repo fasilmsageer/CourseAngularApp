b1366236b48c5f36228b366adbc539e2559ab19b
fasilmsageer <fasilmsageer@gmail.com>
2022-11-30 10:05:33 +0530

    add

--- src/app/add-course/add-course.component.ts
+++ src/app/add-course/add-course.component.ts
@@ -1,4 +1,6 @@
+
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-add-course',
@@ -13,11 +15,31 @@ export class AddCourseComponent {
   courseDate=""
   courseVenue=""
 
+  constructor(private api:ApiService){}
+
 
   readValue=()=>
   {
     let data:any={"courseTitle":this.courseTitle,"courseDescription":this.courseDescription,"courseDuration":this.courseDuration,"courseDate":this.courseDate,"courseVenue":this.courseVenue}
     console.log(data)
+    
+    this.api.addCourses(data).subscribe(
+      (response:any)=>{
+        console.log(response)
+        if (response.status=="success"){
+          alert("Course Added Successfully")
+          this.courseTitle=""
+          this.courseDescription=""
+          this.courseDuration=""
+          this.courseVenue=""
+          this.courseDate=""
+        } else{
+          alert("something went wrong")
+
+        }
+        
+      }
+    )
   }
 
 }
